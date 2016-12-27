73e1a2103d5a6f476491fd8a8aa1609d7b996088
Fernan <Fernan@192.168.1.37>
2016-12-27 17:21:15 +0100

    Comentario
    Prueba

--- BuscaminasIS/src/packCodigo/Main.java
+++ BuscaminasIS/src/packCodigo/Main.java
@@ -4,6 +4,7 @@ import packVentanas.VLogin;
 
 public class Main {
 
+	//ESTE ES EL COMENTARIO
 	public static void main(String[] args) throws NoArchivoAudioException {
 		VLogin vL = new VLogin();
 		vL.setVisible(true);
