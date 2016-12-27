aa26bfb2b0a56853d52bfac027d895731930fe19
Galder <Galder@192.168.1.34>
2016-12-27 17:34:40 +0100

    Prueba commit

--- BuscaminasIS/src/packCodigo/Main.java
+++ BuscaminasIS/src/packCodigo/Main.java
@@ -5,6 +5,7 @@ import packVentanas.VLogin;
 public class Main {
 
 	public static void main(String[] args) throws NoArchivoAudioException {
+		//alert("Bienvenidos");
 		VLogin vL = new VLogin();
 		vL.setVisible(true);
 	}
